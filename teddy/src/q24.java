b12dd93a4384386c3ba8a61e8738f6837fe75fa3
vitramsetu <134389761+vitramsetu@users.noreply.github.com>
2023-05-24 11:43:35 +0530

    q24.java

--- teddy/src/q24.java
+++ teddy/src/q24.java
@@ -10,13 +10,13 @@ class NameNotValidException extends Exception {
     }
 }
 
-class student {
+class stud {
     private int rollNo;
     private String name;
     private int age;
     private String course;
 
-    public student(int rollNo, String name, int age, String course) throws AgeNotWithinRangeException, NameNotValidException {
+    public stud(int rollNo, String name, int age, String course) throws AgeNotWithinRangeException, NameNotValidException {
         if (age < 15 || age > 21) {
             throw new AgeNotWithinRangeException("Age should be between 15 and 21.");
         }
@@ -55,12 +55,12 @@ class student {
 public class q24 {
     public static void main(String[] args) {
         try {
-            student student1 = new student(1, "John Doe", 20, "Science");
+            stud student1 = new stud(1, "John Doe", 20, "Science");
             System.out.println("Student 1: Roll No - " + student1.getRollNo() + ", Name - " + student1.getName() +
                     ", Age - " + student1.getAge() + ", Course - " + student1.getCourse());
 
             // Invalid age
-            student student2 = new student(2, "Jane Smith", 22, "Arts");
+            stud student2 = new stud(2, "Jane Smi1th", 12, "Arts");
             System.out.println("Student 2: Roll No - " + student2.getRollNo() + ", Name - " + student2.getName() +
                     ", Age - " + student2.getAge() + ", Course - " + student2.getCourse());
         } catch (AgeNotWithinRangeException e) {
